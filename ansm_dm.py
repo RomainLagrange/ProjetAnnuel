50411ef9b072fa7b4495b2ed5e2c97c19ba3d897
mpucheu <marion.pucheu@gmail.com>
2019-02-20 23:41:28 +0100

    ansm suite

--- ansm_dm.py
+++ ansm_dm.py
@@ -24,7 +24,65 @@ def main_ansm_dm():
     document.save("soumission-ansm-dm.docx")
 
 def partie_une_ansm_dm(document):
-    doc=document
-    tableIdx=document.add_table(rows=15, cols=1, style='Table Grid')
     
-    x = doc.tables[tableIdx].cell(0,1)._element.xpath('.//w:checkBox')
+    '''Marge de la page'''
+    sections = document.sections
+    for section in sections:
+        section.top_margin = Cm(1.7)
+        section.bottom_margin = Cm(1)
+        section.left_margin = Cm(1)
+        section.right_margin = Cm(1)
+        
+    '''Titre du document'''
+    styles= document.styles
+    style=styles.add_style('debut_page', WD_STYLE_TYPE.PARAGRAPH)
+    paragraph_format = style.paragraph_format
+    paragraph_format.space_before
+    paragraph_format.space_after
+    fontdebut = style.font
+    fontdebut.name = 'Arial'
+    fontdebut.bold = True
+    fontdebut.size = docx.shared.Pt(11) 
+    
+    paragraph=document.add_paragraph("Formulaire de demande d’autorisation auprès de l'ANSM et demande d’avis du comite de protection des personnes (CPP) pour une recherche mentionnée au 1° ou au 2° de l’article L. 1121-1 du code de la santé publique portant sur un dispositif médical (DM) ou un dispositif médical de diagnostic in vitro (DMDIV)\n", style='debut_page')
+    paragraph.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+    
+    paragraph=document.add_paragraph()
+    sentence=paragraph.add_run("Toutes les rubriques du formulaire doivent être complétées.\n")
+    fontdebut = sentence.font
+    fontdebut.name = 'Arial'
+    fontdebut.italic=True
+    fontdebut.size = docx.shared.Pt(10.5)
+    sentence=paragraph.add_run("Partie réservée à l’ANSM / au CPP :\n")
+    fontdebut = sentence.font
+    fontdebut.name = 'Arial'
+    fontdebut.italic=True
+    fontdebut.size = docx.shared.Pt(10)
+    
+    table = document.add_table(rows=3, cols=3, style='Table Grid')
+    a=table.cell(1,0)
+    b=table.cell(2,0)
+    a.merge(b)
+    c=table.cell(1,1)
+    d=table.cell(2,1)
+    c.merge(d)
+    table.cell(0,0).text=("Date de réception de la demande :\n\n\n     /     /     ")
+    table.cell(0,1).text=("Date de demande d’informations complémentaires :\n\n     /     /     ")
+    table.cell(0,2).text=("Refus d’autorisation / avis défavorable : 	\n\n	Préciser la  date :\n     /     /     ")
+    table.cell(1,0).text=("\nDate du début de la procédure :\n\n     /     /     ")
+    table.cell(1,1).text=("Date de réception des informations complémentaires :\n\n     /     /     ")
+    table.cell(1,2).text=("Autorisation / avis favorable : 	\n\nPréciser la date : \n     /     /     ")
+    table.cell(2,2).text=("Retrait de la demande : 	\nDate :      /     /     ")
+    for row in table.rows:
+        for cell in row.cells:
+            paragraphs = cell.paragraphs
+            for paragraph in paragraphs:
+                paragraph.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+                for run in paragraph.runs:
+                    fontdebut = run.font
+                    fontdebut.name = 'Arial'
+                    fontdebut.size = docx.shared.Pt(10)
+    
+    
+    
+    
