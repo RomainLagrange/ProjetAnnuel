c2af4c503337eebe1a79a2387e436009b0202681
mpucheu <marion.pucheu@gmail.com>
2019-02-22 20:10:24 +0100

    en plein D ansm DM

--- ansm_dm.py
+++ ansm_dm.py
@@ -286,4 +286,216 @@ def partie_D(document):
     b=table.cell(1,0)
     a.merge(b)
     
+    paragraph=document.add_paragraph("\nD1. Statut du DM / DM-DIV\n", style='debut_page')
+    paragraph=document.add_paragraph()
+    sentence=paragraph.add_run("Le dispositif est-il marqué CE ?			□ oui   □ non\n\n")
+    fontdebut = sentence.font
+    fontdebut.name = 'Arial'
+    fontdebut.size = docx.shared.Pt(10)
+    fontdebut.bold=True
+    sentence=paragraph.add_run("Si le dispositif est marqué CE, compléter la rubrique ci-dessous ")
+    fontdebut = sentence.font
+    fontdebut.name = 'Arial'
+    fontdebut.size = docx.shared.Pt(10)
+    fontdebut.italic=True
+    
+    table = document.add_table(rows=10, cols=2, style='Table Grid')
+    table.cell(0,0).text=("Nom de l'Organisme Notifié")
+    table.cell(1,0).text=("Numéro de l'Organisme Notifié")
+    table.cell(2,0).text=("Date du marquage CE")
+    table.cell(2,1).text=("     /     /     ")
+    table.cell(3,0).text=("Destination(s) du marquage CE (telles que mentionnées dans la notice) ")
+    table.cell(4,0).text=("Destination(s) du dispositif dans l’essai  ")
+    table.cell(5,0).text=("Est-ce que l’utilisation du dispositif, dans le cadre de la recherche, se fait dans l’indication de son marquage CE ?")
+    table.cell(5,1).text=("□ oui   □ non")
+    table.cell(6,0).text=("La destination du dispositif dans l’essai est-elle conforme à des recommandations publiées (HAS, ANSM, Sociétés savantes, etc..) ?")
+    table.cell(6,1).text=("□ oui   □ non   □ NA")
+    table.cell(7,0).text=("Si oui, citer les références : ")
+    table.cell(8,0).text=("Le dispositif est-il commercialisé dans un Etat membre de la Communauté européenne ou dans un pays tiers ? ")
+    table.cell(8,1).text=("□ oui   □ non")
+    table.cell(9,0).text=("Si oui, citer les pays concernés  ")
+    n=0
+    for col in table.columns:
+        for cell in col.cells:
+            paragraphs = cell.paragraphs
+            for paragraph in paragraphs:
+                if n>9:
+                    paragraph.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+                for run in paragraph.runs:
+                    fontdebut = run.font
+                    fontdebut.name = 'Arial'
+                    fontdebut.size = docx.shared.Pt(10)
+                    n=n+1
+    
+    paragraph=document.add_paragraph("\nD2. Identification du dispositif à étudier \n", style='debut_page')
+    paragraph=document.add_paragraph()
+    sentence=paragraph.add_run("Rubriques à compléter dans tous les cas")
+    fontdebut = sentence.font
+    fontdebut.name = 'Arial'
+    fontdebut.size = docx.shared.Pt(10)
+    fontdebut.italic=True
+    
+    table = document.add_table(rows=10, cols=2, style='Table Grid')
+    table.cell(0,0).text=("Dénomination commune \n(exemple : stent artériel…)")
+    table.cell(1,0).text=("Dénomination commerciale")
+    table.cell(2,0).text=("Modèle")
+    table.cell(3,0).text=("Version (y compris version du logiciel) ")
+    table.cell(4,0).text=("Classification européenne")
+    table.cell(5,0).text=("Classe du DM :\n")
+    table.cell(5,1).text=("Classe du DMDIV :\n")
+    table.cell(6,0).text=("Classe I                                                    □\nClasse IIa invasif à long terme                 □\n"
+                          "Autres IIa                                                  □\nClasse IIb                                                 □\n"
+                          "Classe III                                                  □\nDMIA                                                        □\n")
+    table.cell(6,1).text=("Hors annexe II      		□\nAnnexe II liste A    		□\n"
+                          "Annexe II liste B    		□\nAutotest                              	□\n")
+    a=table.cell(7,0)
+    b=table.cell(7,1)
+    a.merge(b)
+    table.cell(7,0).text=("En cas de dispositif non pourvu du marquage CE, renseigner la classe du dispositif et joindre une justification de la classification ")
+    table.cell(8,0).text=("S’agit-il d’un dispositif implantable ?")
+    table.cell(8,1).text=("□ oui   □ non")
+    table.cell(9,0).text=("S’agit-il d’un dispositif « sur mesure » ?")
+    table.cell(9,1).text=("□ oui   □ non")
+    n=0
+    for row in table.rows:
+        for cell in row.cells:
+            paragraphs = cell.paragraphs
+            for paragraph in paragraphs:
+                if n==12 or n==14:
+                    paragraph.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+                for run in paragraph.runs:
+                    fontdebut = run.font
+                    fontdebut.name = 'Arial'
+                    fontdebut.size = docx.shared.Pt(10)
+                    if n==5 or n==6:
+                        fontdebut.bold=True
+                    elif n==9:
+                        fontdebut.italic=True
+                    n=n+1
+    c=table.cell(5,0)
+    d=table.cell(6,0)
+    c.merge(d)
+    e=table.cell(5,1)
+    f=table.cell(6,1)
+    e.merge(f)
+    
+    paragraph=document.add_paragraph()
+    
+    table = document.add_table(rows=6, cols=2, style='Table Grid')
+    table.cell(0,0).text=("Fabricant du dispositif à étudier ")
+    table.cell(0,1).text=("(à compléter quel que soit le statut du promoteur) ")
+    table.cell(1,0).text=("Nom")
+    table.cell(2,0).text=("Adresse")
+    table.cell(3,0).text=("Numéro de téléphone")
+    table.cell(4,0).text=("Numéro de télécopie")
+    table.cell(5,0).text=("Courriel")
+    n=0
+    for row in table.rows:
+        for cell in row.cells:
+            paragraphs = cell.paragraphs
+            for paragraph in paragraphs:
+                for run in paragraph.runs:
+                    fontdebut = run.font
+                    fontdebut.name = 'Arial'
+                    fontdebut.size = docx.shared.Pt(10)
+                    if n==0:
+                        fontdebut.bold=True
+                    n=n+1
+    a=table.cell(0,0)
+    b=table.cell(0,1)
+    a.merge(b)
+    
+    paragraph=document.add_paragraph()
+    
+    table = document.add_table(rows=5, cols=3, style='Table Grid')
+    table.cell(0,0).text=("Le dispositif sur lequel porte la recherche contient-il une des substances suivantes :")
+    table.cell(0,1).text=("oui")
+    table.cell(0,2).text=("non")
+    table.cell(1,0).text=("-	Substance qui, si elle est utilisée séparément, est susceptible d'être considérée comme un médicament ? ")    
+    table.cell(2,0).text=("-	Produits d’origine biologique (DMOA) ou dans la fabrication duquel interviennent de tels produits ?")
+    table.cell(3,0).text=("-	OGM ?")
+    table.cell(4,0).text=("-	Radioélément ?")
+    table.cell(1,1).text=("□")
+    table.cell(1,2).text=("□")
+    table.cell(2,1).text=("□")
+    table.cell(2,2).text=("□")
+    table.cell(3,1).text=("□")
+    table.cell(3,2).text=("□")
+    table.cell(4,1).text=("□")
+    table.cell(4,2).text=("□")
+    n=0
+    for row in table.rows:
+        for cell in row.cells:
+            paragraphs = cell.paragraphs
+            for paragraph in paragraphs:
+                if n!=0 or n!=3 or n!=6 or n!=9 or n!=12:
+                   paragraph.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER 
+                for run in paragraph.runs:
+                    fontdebut = run.font
+                    fontdebut.name = 'Arial'
+                    if n==0:
+                        fontdebut.bold=True
+                        fontdebut.size = docx.shared.Pt(11)
+                    elif n==1 or n==2:
+                        fontdebut.size = docx.shared.Pt(11)
+                    elif n==3 or n==6 or n==9 or n==12:
+                        fontdebut.size = docx.shared.Pt(10)
+                    else:
+                        fontdebut.size = docx.shared.Pt(10.5)
+                    n=n+1
+    a=table.cell(0,0)
+    b=table.cell(1,0)
+    a.merge(b)
+    c=table.cell(2,0)
+    a.merge(c)
+    d=table.cell(3,0)
+    a.merge(d)
+    e=table.cell(4,0)
+    a.merge(e)
+    
+    paragraph=document.add_paragraph("\nD3. Cas particulier : utilisation de dispositifs à étudier commercialisés et ayant la même dénomination commune, dans le cadre d’un essai dont le protocole n’impose pas l’utilisation d’un dispositif en particulier", style='debut_page')
+    paragraph=document.add_paragraph()
+    sentence=paragraph.add_run("\nEst-ce que ce cas particulier est applicable à l’essai concerné ?			□ oui   □ non\n")
+    fontdebut = sentence.font
+    fontdebut.name = 'Arial'
+    fontdebut.size = docx.shared.Pt(10)
+    sentence=paragraph.add_run("Si oui,  compléter la rubrique ci-dessous")
+    fontdebut = sentence.font
+    fontdebut.name = 'Arial'
+    fontdebut.size = docx.shared.Pt(10)
+    fontdebut.italic=True
+    
+    table = document.add_table(rows=3, cols=3, style='Table Grid')
+    table.cell(0,1).text=("oui")
+    table.cell(0,2).text=("non")
+    table.cell(1,0).text=("DM")
+    table.cell(1,1).text=("□")
+    table.cell(1,2).text=("□")
+    table.cell(2,0).text=("DMDIV")
+    table.cell(2,1).text=("□")
+    table.cell(2,2).text=("□")
+    n=0
+    for row in table.rows:
+        for cell in row.cells:
+            paragraphs = cell.paragraphs
+            for paragraph in paragraphs:
+                if n==0 or n==1 or n==3 or n==4 or n==6 or n==7:
+                   paragraph.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER 
+                for run in paragraph.runs:
+                    fontdebut = run.font
+                    fontdebut.name = 'Arial'
+                    fontdebut.size = docx.shared.Pt(11)
+                    n=n+1
+    
+    
+    paragraph=document.add_paragraph()
+    sentence=paragraph.add_run("Dans l’affirmative, préciser les informations mentionnées ci-après")
+    fontdebut = sentence.font
+    fontdebut.name = 'Arial'
+    fontdebut.size = docx.shared.Pt(10)
+    fontdebut.italic=True
+    
+    table = document.add_table(rows=5, cols=4, style='Table Grid')
+    
+    
     
