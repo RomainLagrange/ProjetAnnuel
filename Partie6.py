384347cda9c3a82f3c5f878d2cc9c82bc90e2d50
Julie-GPhy <jujunat@orange.fr>
2019-02-28 17:15:04 +0100

    suite texte protocole1

--- Partie6.py
+++ Partie6.py
@@ -25,7 +25,7 @@ from docx.shared import Cm, Pt, RGBColor, Inches
 def Partie6(document):
 #def Partie6():
     'Creation de la partie 6 du protcole de catégorie 1'
-  #  document = docx.Document()
+   # document = docx.Document()
 
 
 #   Marge de la page
@@ -39,7 +39,7 @@ def Partie6(document):
 #---------------------------DEFINITIONS DES STYLES
  
 
-    #Style(document)
+  #  Style(document)
 
 
 #    
@@ -70,42 +70,8 @@ def Partie6(document):
     # Ecriture du 6.3  
     Titre2('6.3	Visites de pré-inclusion / inclusion = Visite V0',document)
     
-#    p=document.add_paragraph()
-#    run1=p.add_run()
-#    run1.text='Lors de la visite de '
-#    run1.style='Paragraphe'
-#    run2=p.add_run()
-#    run2.text='pré-inclusion (voir selon l’étude si visite d’inclusion),'
-#    run2.style='Paragraphe'
-#    run2.font.italic=True
-#    run3=p.add_run()
-#    run3.text='le médecin investigateur informe le patient de la possibilité de participer à cet essai clinique et répond à toutes ses questions concernant l\'objectif, la nature des contraintes, les risques prévisibles et les bénéfices attendus de la recherche. Il précise également les droits du patient dans le cadre d’une recherche et vérifie les critères d’éligibilité. '
-#    run3.style='Paragraphe'
-#    
-#    
-#    document.add_paragraph('Un exemplaire de la note d’information et du formulaire de consentement est alors remis au participant par le médecin investigateur.', style='Paragraphe')
-#    
-#    
-#    p=document.add_paragraph()
-#    run1=p.add_run('Après cette séance d’information, le participant dispose d’un délai de réflexion. Le médecin investigateur est responsable de l’obtention du consentement éclairé écrit du participant.\n Si le participant donne son accord de participation, ce dernier et l’investigateur inscrivent leurs noms et prénoms en clair, datent et signent le formulaire de consentement. Celui-ci ')
-#    run1.style='Paragraphe'
-#    run2=p.add_run('doit être signé avant la réalisation de tout examen ')
-#    run2.style='Paragraphe'
-#    run2.font.bold= True
-#    run2.font.underline= True
-#    run3=p.add_run('clinique ou biologique ou para-clinique nécessité par la recherche. ')
-#    run3.style='Paragraphe'
-#    
-#    p=document.add_paragraph()
-#    run1=p.add_run('L’exemplaire ')
-#    run1.style='Paragraphe'
-#    run2=p.add_run('original ')
-#    run2.style='Paragraphe'
-#    run2.font.underline= True
-#    document.add_paragraph('sera conservé dans le classeur de l’investigateur. Un exemplaire (un autre original ou une copie) sera remis au patient. ', style = 'Paragraphe')
-#    document.add_paragraph('L’investigateur précisera dans le dossier médical du patient sa participation à la recherche, les modalités du recueil du consentement ainsi que celle de l’information. ', style = 'Paragraphe')
-#    
-    #A FINIR 
+
+
     
     #Ecriture du titre6.3.1
     Titre3('6.3.1','Recueil du consentement',document)
@@ -113,12 +79,84 @@ def Partie6(document):
     
     TexteGris('prendre contact avec la promotion interne \n pour aide a la redaction de ce chapitre', document)
     
+    p=document.add_paragraph()
+    run1=p.add_run()
+    run1.text='Lors de la visite de '
+    run1.style='Paragraphe'
+    run2=p.add_run()
+    run2.text='pré-inclusion (voir selon l’étude si visite d’inclusion),'
+    run2.style='Paragraphe'
+    run2.font.italic=True
+    run3=p.add_run()
+    run3.text='le médecin investigateur informe le patient de la possibilité de participer à cet essai clinique et répond à toutes ses questions concernant l\'objectif, la nature des contraintes, les risques prévisibles et les bénéfices attendus de la recherche. Il précise également les droits du patient dans le cadre d’une recherche et vérifie les critères d’éligibilité. '
+    run3.style='Paragraphe'
+    
+    
+    p=document.add_paragraph()
+    run1=p.add_run()
+    run1.text=('Un exemplaire de la note d’information et du formulaire de consentement est alors remis au participant par le médecin investigateur.')
+    run1.style='Paragraphe'
+    
+    p=document.add_paragraph()
+    run1=p.add_run('Après cette séance d’information, le participant dispose d’un délai de réflexion. Le médecin investigateur est responsable de l’obtention du consentement éclairé écrit du participant.\n Si le participant donne son accord de participation, ce dernier et l’investigateur inscrivent leurs noms et prénoms en clair, datent et signent le formulaire de consentement. Celui-ci ')
+    run1.style='Paragraphe'
+    run2=p.add_run('doit être signé avant la réalisation de tout examen ')
+    run2.style='Paragraphe'
+    run2.font.bold= True
+    run2.font.underline= True
+    run3=p.add_run('clinique ou biologique ou para-clinique nécessité par la recherche. ')
+    run3.style='Paragraphe'
+    
+    p=document.add_paragraph()
+    run1=p.add_run('L’exemplaire ')
+    run1.style='Paragraphe'
+    run2=p.add_run('original ')
+    run2.style='Paragraphe'
+    run2.font.underline= True
+    
+    p=document.add_paragraph()
+    run1=p.add_run('sera conservé dans le classeur de l’investigateur. Un exemplaire (un autre original ou une copie) sera remis au patient. ')
+    run1.style='Paragraphe'
+    
+    p=document.add_paragraph()
+    run1=p.add_run('L’investigateur précisera dans le dossier médical du patient sa participation à la recherche, les modalités du recueil du consentement ainsi que celle de l’information. ')
+    run1.style='Paragraphe'
+    
+    
+    p=document.add_paragraph()
+    run1=p.add_run('Décrire le processus de numérotation du patient, par exemple : \n')
+    run1.style='Paragraphe'
+    run1.font.italic=True
+    run2=p.add_run('Le patient se verra attribuer un numéro de patient, selon la règle : ')
+    run2.style='Paragraphe'
+    
+        #IMAGE NUMERO PATIENT
 
-    #TEXTE
+    p=document.add_paragraph()
+    run1=p.add_run('Procédure d’urgence si applicable')
+    run1.style='Paragraphe'
+    run1.font.italic=True
+    run1.font.underline=True
+
+    p=document.add_paragraph()
+    run1=p.add_run('Dans le cas d’une situation d’urgence et conformément à l’article L.1122-1-2, le consentement sera sollicité auprès de « proches » et seulement rétrospectivement auprès du patient dès une récupération suffisante lui permettant de donner son consentement libre et éclairé. ')
+    run1.style='Paragraphe'
+    
+    
+    p=document.add_paragraph()
+    run1=p.add_run('Dans le cas où les « proches » ne peuvent pas être présents au moment de l’inclusion, une procédure d’urgence sera mise en place dans le cas d’une urgence vitale immédiate. Dans ce cas un médecin indépendant de l’étude, non déclaré comme médecin investigateur, peut donner son consentement d’urgence. L\'intéressé, ou le cas échéant, les membres de la famille ou la personne de confiance sont informés dès que possible et leur consentement leur est demandé pour la poursuite de cette recherche.')
+    run1.style='Paragraphe'
     
     #Ecriture du titre6.3.2
     Titre3('6.3.2','Déroulement de la visite',document)
 
+    document.add_paragraph()
+    run1=p.add_run('La visite de pré-inclusion/inclusion est assurée par le médecin investigateur. La visite de pré-inclusion a lieu entre X jours/semaines/mois et au plus tard X jours/semaines/mois avant la visite d’inclusion.')
+    run1.style='Paragraphe'
+    
+    document.add_paragraph()
+    run1=p.add_run('Avant tout examen lié à la recherche, l’investigateur recueille le consentement libre, éclairé et écrit du participant (ou de son représentant légal le cas échéant).')
+    run1.style='Paragraphe'
     
     #Ecriture du titre 6.4
     Titre2('6.4	Visite de randomisation = Visite (Vx, ou Jx, ou Mx…)',document)
@@ -130,6 +168,23 @@ def Partie6(document):
     #Ecriture du titre6.4.2
     Titre3('6.4.2','Randomisation du patient',document)
 
+    p=document.add_paragraph()
+    run1=p.add_run('Lorsqu’un investigateur souhaite effectuer la randomisation/l’inclusion après avoir vérifié l’éligibilité du participant/cluster, il se connecte sur le site Internet de l’e-CRF https://www.chu-poitiers.hugo-online.fr/. L’investigateur complète la page « randomisation » après avoir préalablement confirmé tous les critères d’éligibilité du participant/cluster sur le site. Après validation du contenu, la randomisation/ l’inclusion est effectuée et l’e-CRF :')
+    run1.style='Paragraphe'
+
+    p=document.add_paragraph()
+    run1=p.add_run('Si l’étude est en ouvert : communique immédiatement à l’investigateur en clair le résultat de la randomisation/l’inclusion, en particulier le groupe de traitement /stratégie/procédure alloué(e) au participant/cluster et le numéro de la boîte de traitement.')
+    run1.style='Paragraphe'
+
+    #FINIR 
+#    p=document.add_paragraph()
+#    run1=p.add_run()
+#    run1.style='Paragraphe'
+#
+#    p=document.add_paragraph()
+#    run1=p.add_run()
+#    run1.style='Paragraphe'
+#    document.add_paragraph('',style='Paragraphe')
 
 
     #Ecriture du titre 6.5
@@ -146,6 +201,10 @@ def Partie6(document):
     #Ecriture du titre 6.6
     Titre2('6.6	Visite de fin de la recherche',document)
     
+#    p=document.add_paragraph()
+#    run1=p.add_run()
+#    run1.style='Paragraphe'
+    
     #Ecriture du titre 6.7
     Titre2('6.7	Règles d’arrêt de la participation d’une personne à la recherche',document)
 
@@ -155,6 +214,26 @@ def Partie6(document):
     #Ecriture du titre6.7.1
     Titre3('6.7.1','Arrêt de participation définitif ou temporaire d’un patient dans l’étude)',document)
 
+#    p=document.add_paragraph()
+#    run1=p.add_run()
+#    run1.style='Paragraphe'
+#    
+#    p=document.add_paragraph()
+#    run1=p.add_run()
+#    run1.style='Paragraphe'
+#
+#    p=document.add_paragraph()
+#    run1=p.add_run()
+#    run1.style='Paragraphe'
+#
+#    p=document.add_paragraph()
+#    run1=p.add_run()
+#    run1.style='Paragraphe'
+#
+#    p=document.add_paragraph()
+#    run1=p.add_run()
+#    run1.style='Paragraphe'
+
 
     #Ecriture du titre6.7.2
     Titre3('6.7.2','Modalités de remplacement des patients exclus, le cas échéant',document)
@@ -163,9 +242,16 @@ def Partie6(document):
     #Ecriture du titre6.7.3
     Titre3('6.7.3','Modalités et calendrier de recueil pour ces données',document)
 
+#    p=document.add_paragraph()
+#    run1=p.add_run()
+#    run1.style='Paragraphe'
     
     #Ecriture du titre6.7.4
     Titre3('6.7.4','Modalités de suivi de ces personnes',document)
+
+#    p=document.add_paragraph()
+#    run1=p.add_run()
+#    run1.style='Paragraphe'
 
     #Ecriture du titre 6.8
     Titre2('6.8	Contraintes liées à la recherche et indemnisation éventuelle des participants',document)
@@ -192,10 +278,21 @@ def Partie6(document):
     #Ecriture du titre 6.10
     Titre2('6.10	Arrêt d’une partie ou de la totalité de la recherche',document)
     
-    
+#    p=document.add_paragraph()
+#    run1=p.add_run()
+#    run1.style='Paragraphe'
+#    
+#    p=document.add_paragraph()
+#    run1=p.add_run()
+#    run1.style='Paragraphe'
+#
+#    p=document.add_paragraph()
+#    run1=p.add_run()
+#    run1.style='Paragraphe'
+
     #FIN DU DOC 
     paragraph = document.add_paragraph()
     run = paragraph.add_run()
     run.add_break(WD_BREAK.PAGE)
   
-   # document.save("Partie6.docx")   
+  #  document.save("Partie6.docx")   
